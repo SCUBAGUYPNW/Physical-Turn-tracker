b3bd8e44e81fcdf9a2ea3402212ea10183686f43
scubaguypnw <scubaphoto@gmail.com>
2021-01-04 21:07:55 -0800

    Add project files.

---
+++ Turn tracker.ino
@@ -0,0 +1,33 @@
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Turn tracker.ino
+    Created:	1/4/2021 20:58:56
+    Author:     ROG-CODING-LAPT\scuba
+*/
+
+// Define User Types below here or use a .h file
+//
+
+
+// Define Function Prototypes that use User Types below here or use a .h file
+//
+
+
+// Define Functions below here or use other .ino or cpp files
+//
+
+// The setup() function runs once each time the micro-controller starts
+#include <Wire.h>
+void setup()
+{
+
+
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+
+
+}
