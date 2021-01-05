36daf471705fc5e2c0092d52c731eb121f060aed
scubaguypnw <scubaphoto@gmail.com>
2021-01-04 21:15:37 -0800

    Added variables

--- Turn tracker.ino
+++ Turn tracker.ino
@@ -21,6 +21,41 @@
 #include <FastLED.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
+
+//#define NUM_LEDS 15
+
+
+
+
+// Define LCD pinout
+const int  en = 2, rw = 1, rs = 0, d4 = 4, d5 = 5, d6 = 6, d7 = 7, bl = 3;
+
+// Define I2C Address - change if reqiuired
+const int i2c_addr = 0x3F;
+
+LiquidCrystal_I2C lcd(0x27, en, rw, rs, d4, d5, d6, d7, bl, POSITIVE);
+int buttonNextTurn = 10;
+int buttonNextRound = 11;
+int buttonUndo = 12;
+int buttonValTurn;
+int buttonValRound;
+int buttonValUndo;
+int dt = 1000;
+int turnCount = 0;
+int roundCount = 0;
+int displayValTurn = 0;
+int displayValRound = 0;
+int turnUndoVal = 0;
+int roundUndoVal = 0;
+int lastButtonPress = 0;
+const int ledPin = 8;
+int currentLED = 9;
+int undoLED = 0;
+
+//CRGB leds[NUM_LEDS];
+
+
+
 void setup()
 {
 
